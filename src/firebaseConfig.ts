655b3be614f194bf0699b6814fd653b051ca29fb
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2022-09-21 17:23:56 +0530

    [build] - packages required

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJUyYO67DdYCl-qHV0SWEWhjc7j_qSUDg",
+  authDomain: "tomato-5ae9e.firebaseapp.com",
+  projectId: "tomato-5ae9e",
+  storageBucket: "tomato-5ae9e.appspot.com",
+  messagingSenderId: "486705980894",
+  appId: "1:486705980894:web:7fa04b4759207d6d2c694f",
+  measurementId: "G-D5V34XP72T"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
